b18b4723a75dea6942c217621b853ac118387ba0
Cole Vanacore <colevanacore@gmail.com>
2019-09-11 14:28:22 -0400

    Create components for header and calendar

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                Countdown 2 Race Day
+            </h1>
+        </div>
+    )
+}
+
+export default Header;
